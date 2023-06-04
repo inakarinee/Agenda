96e13e61e0f5c2ff4cfca65e168d36afb7789e9f
Inaê Marchese <112870806+inakarinee@users.noreply.github.com>
2023-06-04 16:44:02 -0300

    Update cadastro.js

--- cadastro.js
+++ cadastro.js
@@ -1,38 +1,65 @@
 let contador = 0
 let atividades = [];
-const contadorElement = document.getElementById("contador")
+let horarios = [];
+let contadorElement = document.getElementById("contador");
+
+// function exibirlista() {
+//     let exibir = document.getElementById("tarefas")
+//     for (item in lista (atividades)){
+//         itemLista = document.createElement("p")
+//         itemLista.innerHTML = itemdiv.appendChild(itemLista)
+// }
+// }
 
 function cadastro_tarefas(){
     let atividade = document.getElementById("atividade").value;
     let siape = document.getElementById("siape");
     let dia = document.getElementById("dia");
     let carga_horaria = document.getElementById("carga").value;
-    let horario_inicial = document.getElementById("horario_inicial").value;
-    let horario_final = document.getElementById("horario_final").value;
+    let horario_inicial = document.getElementById("horario_inicial").value.split(":");
+    let horario_final = document.getElementById("horario_final").value.split(":");
 
-    // const horas_finais = []
-    // for(let i = 0; i <= carga_horaria; horas_finais++)
-    // horas_finais.push{
+    let horario_inicial_min = (Number(horario_inicial[0] * [60])) + Number(horario_inicial[1]) ;
+    let horario_final_min = (Number(horario_final[0] * [60])) + Number(horario_final[1]) ;
+    let carga_min = (Number(carga_horaria[0] * 60)) + Number(carga_horaria[1]);
+
         lista_de_atividades(atividade);
-        horas(horario_inicial, horario_final, carga_horaria);
+        horas(horario_inicial_min, horario_final_min, carga_horaria, carga_min);
+        horario(horario_inicial, horario_final);
+        // exibirlista(atividades)
 
+    }
+
+    function horario(horario_inicial, horario_final){
+        horarios.push(horario_inicial, horario_final)
+        document.querySelector("#horario").innerHTML = horarios;
     }
 
     function lista_de_atividades(atividade) {
         atividades.push(atividade);
         console.log(atividades)
-    }
+        document.querySelector("#tarefa").innerHTML = atividades;
+}
 
-    function horas(horario_inicial, horario_final, carga_horaria) {
-        contador += parseFloat(horario_final) - parseFloat(horario_inicial);
-        if (contador >= carga_horaria){
-            alert("Vc já concluiu sua carga horária")
-            contadorElement.innerHTML = `${carga_horaria}/${carga_horaria}`
+    function horas(horario_inicial_min, horario_final_min, carga_horaria, carga_min) {
+        contador += parseFloat(horario_final_min) - parseFloat(horario_inicial_min);
+        let contador_hora = contador / 60
+        if (contador >= carga_min){
+            alert("Você já concluiu sua carga horária")
+            contadorElement.innerHTML = `${carga_horaria}/${carga_horaria}`;
         }
         else{
-            contadorElement.innerHTML = `${contador}/${carga_horaria}`
+            contadorElement.innerHTML = `${contador_hora}/${carga_horaria}`
         }
     }
+
+
+//     if (horario == horario && dia == dia){
+//         alert("Este horário já está preenchido")}
+// }
+
+
+    
 
 
 //     if (cadastro.usuario == nome_docente)
@@ -44,5 +71,8 @@ function cadastro_tarefas(){
 //     if (horario == horario && dia == dia){
 //         alert("Este horário já está preenchido")}
 // }
+
+
+
 
 
